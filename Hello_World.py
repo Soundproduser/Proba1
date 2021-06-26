c9e1d654cc51ac9706ffcca1882740224503bcd2
Soundproduser <86266417+Soundproduser@users.noreply.github.com>
2021-06-26 15:14:15 +0300

    Random output

--- Hello_World.py
+++ Hello_World.py
@@ -1,7 +1,23 @@
-print("Hello world")
-print("Hello world")
+# print("Hello world")
+# print("Hello world")
 
 #- написать код который выведет текущее время в момент запуска скрипта.
 
+# import datetime
+# print(datetime.datetime.today())
+
 import datetime
-print(datetime.datetime.today())
+
+d1=datetime.datetime.today()
+
+import random
+
+what=(random.random())
+
+if what >0.5:
+    print("Tomorrow:" + str(d1+datetime.timedelta(days=1)))
+else:
+    print("yesterday:" + str(d1+datetime.timedelta(days=-1)))
+
+
+
