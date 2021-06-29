3491b8c4903452676dc1201733ff551a6e5ba319
Konstantin Maruhnich <nocturneumbra@gmail.com>
2021-06-29 21:26:38 +0300

    Training for solving code conflicts
    Add http client and sending http request to google.com

--- Hello_World.py
+++ Hello_World.py
@@ -6,12 +6,18 @@
 # import datetime
 # print(datetime.datetime.today())
 
+import http.client
+
+
 import datetime
 
 d1=datetime.datetime.today()
 
 import random
-
+conn = http.client.HTTPSConnection("google.com")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 what=(random.random())
 
 if what >0.5:
